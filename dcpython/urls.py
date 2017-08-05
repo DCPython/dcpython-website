ee61fe419aef5035930bfc2a0693cbca3284b4bb
Alex Clark <aclark@aclark.net>
2017-08-05 11:51:30 -0400

    Update

--- dcpython/urls.py
+++ dcpython/urls.py
@@ -19,6 +19,7 @@ from .website import views
 
 urlpatterns = [
     url(r'^about$', views.about, name='about'),
+    url(r'^team$', views.team, name='team'),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
 ]
