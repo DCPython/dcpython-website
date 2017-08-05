a497dc56cbbc3a5860d39d7f5f8d8dc2d3ab9374
Alex Clark <aclark@aclark.net>
2017-08-05 05:18:38 -0400

    Update

--- dcpython/wsgi.py
+++ dcpython/wsgi.py
@@ -15,6 +15,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcpython.settings")
 
 application = get_wsgi_application()
 
-# Whitenoise
+# WhiteNoise
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
